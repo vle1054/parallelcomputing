8a0a9a0ff50eed1c930300f4520be8e6e3288dee
VinhLe <vle1054@gmail.com>
2018-04-25 14:37:28 -0600

    chang

--- homework4/homework4_Vinh_Le.cu
+++ homework4/homework4_Vinh_Le.cu
@@ -14,25 +14,7 @@ using namespace std;
 #define TOLERANCE 0.001
 
 __global__ void scan (int * arr, int * arr_gpu, int n) {
-  extern __shared__ float temp[]; // allocated on invocation
-   int thid = threadIdx.x;
-  int pout = 0, pin = 1;
-  // Load input into shared memory.
-   // This is exclusive scan, so shift right by one
-   // and set first element to 0
-  temp[pout*n + thid] = (thid > 0) ? arr[thid-1] : 0;
-  __syncthreads();
-  for (int offset = 1; offset < n; offset *= 2)
-  {
-    pout = 1 - pout; // swap double buffer indices
-    pin = 1 - pout;
-    if (thid >= offset)
-      temp[pout*n+thid] += temp[pin*n+thid - offset];
-    else
-      temp[pout*n+thid] = temp[pin*n+thid];
-    __syncthreads();
-  }
-  arr_gpu[thid] = temp[pout*n+thid]; // write output
+
 
 
 }
@@ -42,14 +24,15 @@ int main(int argc, char *argv[]){
 srand(time(NULL));
 
 int n = atoi(argv[1]);
-int arr[n];
+
 //Generate array
 cout<<"Generating "<<n<< " random numbers"<<endl;
 
-int  * arr_cpu, * arr_gpu;
-
+int *arr, * arr_cpu, * arr_gpu;
+arr = (int *) malloc(n*sizeof(int))
 arr_cpu = (int *) malloc(n*sizeof(int));
 arr_gpu = (int *) malloc(n*sizeof(int));
+cout<<"Size of Array"<<sizeof(arr)<<endl;
 
 //fill arr with rnd nums between 1-1000
 for (int i = 0; i<n; i++){
@@ -69,21 +52,6 @@ for (int i=1; i<n; i++) {
 
 cout<<"GPU SCAN"<<endl;
 
-//initialize and allocate memory for device same set as host
-int * arr_d, * arr_gpu_d;
-
-
-cudaMalloc((void**) & arr_d, n*sizeof(int));
-cudaMalloc((void**) & arr_gpu_d, n*sizeof(int));
-
-
-//copy data from host to device
-cudaMemcpy(arr_d, arr, n*sizeof(int), cudaMemcpyHostToDevice);
-
-//GPU SCAN
-scan<<<n, 32>>>(arr_d, arr_gpu_d, n);
-//copy data from device to host
-cudaMemcpy(arr_gpu, arr_gpu_d, n*sizeof(float), cudaMemcpyDeviceToHost);
 
 for(int i = 0; i<sizeof(arr_cpu)-1;i++){
 cout<<arr_cpu[i]<<",";
