36ae4879594718b285dd084ac5ec39fe1e491b31
VinhLe <vle1054@gmail.com>
2018-04-25 13:33:21 -0600

    changes

--- homework4/homework4_Vinh_Le.cu
+++ homework4/homework4_Vinh_Le.cu
@@ -25,10 +25,11 @@ using namespace std;
 #define CONFLICT_FREE_OFFSET(n) ((n) >> LOG_NUM_BANKS)
 #endif
 
-__global__ void SCAN (int * arr, int * arr_gpu, int * n) {
+__global__ void SCAN (int * arr, int * arr_gpu, int * n_d) {
   extern __shared__ float temp[]; // allocated on invocation
   int thid = threadIdx.x;
   int pout = 0, pin = 1;
+  int n = &n_d;
   // load input into shared memory.
   // Exclusive scan: shift right by one and set first element to 0
   temp[thid] = (thid > 0) ? q[thid-1] : 0;
