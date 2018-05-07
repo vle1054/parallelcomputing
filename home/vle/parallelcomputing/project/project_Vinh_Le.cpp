f5376353743b6d973222cfa9fbe9f1dcd12345d5
vle1054 <vle1054@gmail.com>
2018-05-06 18:33:49 -0600

    delete

--- home/vle/parallelcomputing/project/project_Vinh_Le.cpp
+++ home/vle/parallelcomputing/project/project_Vinh_Le.cpp
@@ -23,96 +23,129 @@ Parallelization Method: CilkPlus
 
 using namespace std;
 
-void sswap(int* a, int* b){
+void swap(int* a, int* b){
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-int spartition (int arr[], int low, int high){
-    int pivot = arr[high];
-    int i = (low - 1);
+int partition (int arr[], int low, int high){
+  int pivot = arr[high];
+  int i = (low - 1);
 
-    for (int j = low; j <= high- 1; j++){
-        if (arr[j] <= pivot){
-            i++;
-            sswap(&arr[i], &arr[j]);
-        }
+  for (int j = low; j <= high- 1; j++){
+    if (arr[j] <= pivot){
+      i++;
+      swap(&arr[i], &arr[j]);
     }
-    sswap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+  }
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
 }
 
 void sequential_quickSort(int arr[], int low, int high){
-    if (low < high){
+  if (low < high){
+    int pi = partition(arr, low, high);
 
-        int pi = spartition(arr, low, high);
-
-        sequential_quickSort(arr, low, pi - 1);
-        sequential_quickSort(arr, pi + 1, high);
-    }
+    sequential_quickSort(arr, low, pi - 1);
+    sequential_quickSort(arr, pi + 1, high);
+  }
 }
 
-void parallel_quicksort(int arr[], int low, int high){
-    if (low < high){
+void parallel_front_quicksort(int arr[], int low, int high){
+  if (low < high){
+    int pi = partition(arr, low, high);
 
-        int pi = spartition(arr, low, high);
+    cilk_spawn parallel_front_quicksort(arr, low, pi - 1);
+    parallel_front_quicksort(arr, pi + 1, high);
+    cilk_sync;
+  }
+}
 
-        cilk_spawn parallel_quicksort(arr, low, pi - 1);
-         parallel_quicksort(arr, pi + 1, high);
-        cilk_sync;
-    }
+void parallel_back_quicksort(int arr[], int low, int high){
+  if (low < high){
+    int pi = partition(arr, low, high);
+
+     parallel_back_quicksort(arr, low, pi - 1);
+    cilk_spawn parallel_back_quicksort(arr, pi + 1, high);
+    cilk_sync;
+  }
+}
+
+void parallel_both_quicksort(int arr[], int low, int high){
+  if (low < high){
+    int pi = partition(arr, low, high);
+
+    cilk_spawn parallel_both_quicksort(arr, low, pi - 1);
+    cilk_spawn parallel_both_quicksort(arr, pi + 1, high);
+    cilk_sync;
+  }
 }
 
 int main(int argc, char *argv[]){
-    float tdiff1, tdiff2;
+  float tdiff1, tdiff2;
 
-	   srand(time(NULL));
+  srand(time(NULL));
 
-	  int n = atoi(argv[1]);
+  int n = atoi(argv[1]);
 
-    int* a = new int[n];
-    int* b = new int[n];
-    cilk_for (int i = 0; i< n; i++){
-        a[i] = i;
-    }
+  int* a = new int[n];
+  int* b = new int[n];
+  int* c = new int[n];
+  int* d = new int[n];
+
+  cilk_for (int i = 0; i< n; i++){
+    a[i] = i;
+  }
 
 //shuffle
   random_shuffle(a, a + n);
 
   cilk_for (int i = 0; i< n; i++){
-      b[i] = a[i];
+    b[i] = a[i];
+    c[i] = a[i];
+    d[i] = a[i];
   }
-
 
 //sequential sort
   chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
-    sequential_quickSort(a, 0, n-1);
+  sequential_quickSort(a, 0, n-1);
   chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
   chrono::duration<double> time_span1 = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-    printf("Time it took for Sequential run: %3.5f with %d elements\n", time_span1.count(), n);
-
-
-
+  printf("Time it took for Sequential run: %3.5f with %d elements\n", time_span1.count(), n);
 
 //parallel sort
-    chrono::high_resolution_clock::time_point t3 = chrono::high_resolution_clock::now();
-    parallel_quicksort(b, 0, n-1);
-    chrono::high_resolution_clock::time_point t4 = chrono::high_resolution_clock::now();
-    chrono::duration<double> time_span2 = chrono::duration_cast<chrono::duration<double>>(t4 - t3);
-        printf("Time it took for Parallel run: %3.5f with %d elements\n", time_span2.count(), n);
+  chrono::high_resolution_clock::time_point t3 = chrono::high_resolution_clock::now();
+  parallel_front_quicksort(b, 0, n-1);
+  chrono::high_resolution_clock::time_point t4 = chrono::high_resolution_clock::now();
+  chrono::duration<double> time_span2 = chrono::duration_cast<chrono::duration<double>>(t4 - t3);
+  printf("Time it took for Parallel Front run: %3.5f with %d elements\n", time_span2.count(), n);
 
+  chrono::high_resolution_clock::time_point t5 = chrono::high_resolution_clock::now();
+  parallel_back_quicksort(c, 0, n-1);
+  chrono::high_resolution_clock::time_point t6 = chrono::high_resolution_clock::now();
+  chrono::duration<double> time_span3 = chrono::duration_cast<chrono::duration<double>>(t6 - t5);
+  printf("Time it took for Parallel Back run: %3.5f with %d elements\n", time_span3.count(), n);
 
+  chrono::high_resolution_clock::time_point t7 = chrono::high_resolution_clock::now();
+  parallel_both_quicksort(d, 0, n-1);
+  chrono::high_resolution_clock::time_point t8 = chrono::high_resolution_clock::now();
+  chrono::duration<double> time_span4 = chrono::duration_cast<chrono::duration<double>>(t8 - t7);
+  printf("Time it took for Parallel Both run: %3.5f with %d elements\n", time_span4.count(), n);
 
 
 //Check
   bool afail = false;
-    bool bfail = false;
+  bool bfail = false;
+  bool cfail = false;
+  bool dfail = false;
 
-    for(int i = 0; i<n;i++){
-      if (a[i] != i){afail = true;}
-      if (b[i] != i){bfail = true;}
-    }
+  for(int i = 0; i<n;i++){
+    if (a[i] != i){afail = true;}
+    if (b[i] != i){bfail = true;}
+    if (c[i] != i){cfail = true;}
+    if (d[i] != i){dfail = true;}
+  }
 
 
   if(afail == true){
@@ -123,9 +156,21 @@ int main(int argc, char *argv[]){
 
 
   if(bfail == true) {
-    printf("Parallel Sorting Has Failed.\n");
+    printf("Parallel Front Sorting Has Failed.\n");
   }else{
-    printf("Parallel Sorting Has Succeeded.\n" );
+    printf("Parallel Front Sorting Has Succeeded.\n" );
+  }
+
+  if(cfail == true) {
+    printf("Parallel Back Sorting Has Failed.\n");
+  }else{
+    printf("Parallel Back Sorting Has Succeeded.\n" );
+  }
+
+  if(dfail == true) {
+    printf("Parallel Both Sorting Has Failed.\n");
+  }else{
+    printf("Parallel Both Sorting Has Succeeded.\n" );
   }
 
 
